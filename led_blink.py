00595decc242ff70d4d5e0e861580873eb0fe654
ee250 <shmcdono@usc.edu>
2018-02-20 17:53:37 -0800

    led_blink.py take 1

--- led_blink.py
+++ led_blink.py
@@ -1,1 +1,25 @@
 import RPI.GPIO as GPIO
+import time
+
+#set LED port for output
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(17, GPIO.OUT)
+
+def Main()
+	# loop with delay of 0.5s
+	# while True:
+	# 	GPIO.output(17, GPIO.HIGH)
+	# 	time.sleep(0.5)
+	# 	GPIO.output(17, GPIO.LOW)
+	# 	time.sleep(0.5)
+
+	# GPIO.cleanup()
+
+	p = GPIO.PWM(17, 0.125)
+	p.start(1)
+	input('Press return to stop:')
+	p.stop
+	GPIO.clenup()
+
+if __name__ == '__main__'
+	Main()
