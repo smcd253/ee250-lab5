05beb2b8a2d783f0ee654af455d8452807ddd509
ee250 <shmcdono@usc.edu>
2018-02-27 17:16:05 -0800

    simpletest.py

--- simpletest.py
+++ simpletest.py
@@ -17,7 +17,7 @@ import Adafruit_MCP3008
 # mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 # Hardware SPI configuration:
-SPI_PORT   = 21
+SPI_PORT   = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
