cf0be3f2ec52edb4192a646519e8faf846dd66bc
ee250 <shmcdono@usc.edu>
2018-02-27 17:24:47 -0800

    light_sensor.py

--- light_sensor.py
+++ light_sensor.py
@@ -1,20 +1,30 @@
-import RPi.GPIO as GPIO
+# Simple example of reading the MCP3008 analog input channels and printing
+# them all out.
+# Author: Tony DiCola
+# License: Public Domain
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
 
-GPIO.setup(21, GPIO.IN)
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
-def Main():
-	while True:
-		if GPIO.input(21):
-			data = GPIO.input(21)
-		else:
-			print("no data")
-		time.sleep(0.1)
+count = 0
+end = 50
+lightChannel = 0
+threshold = 500
+# Main program loop.
+while count < end:
+	# read light level from pin
+	lightVal = mcp.read_adc(lightChannel)
+    
+    if lightVal < threshold:
+    	print("DARK")
+    else:
+    	print("LIGHT")
+    time.sleep(0.1)
 
-	GPIO.cleanup()
-
-if __name__ == '__main__':
-	Main()
